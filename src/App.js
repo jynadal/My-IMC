88a4a1930ecb4d7c13333631926018a3dc6e9764
jynadal <nadaljeanyves@hotmail.fr>
2020-06-29 23:10:39 +0200

    Before IMC Math

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    weight: undefined,
+    hight: undefined,
+    imc: '123'
+  })
+
+  const handleChange = (evt) => {
+
+    const weight =document.querySelector('[name=weight]').value;
+    const hight =document.querySelector('[name=hight]').value;
+
+    if (!weight || !hight) {
+      return;
+    }else{
+      console.log(weight, hight);
+    }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My IMC</h1>
+        <form>
+          <input onChange={handleChange} name="weight" type="number" placeholder=" Poids en kg" />
+          <input onChange={ handleChange} name="hight" type="number" placeholder=" Taille en m"/>
+          <button>Sauver</button>
+        </form>
+
+        <div className="interpretation">
+          <p>onChange --> imc XX --> interpretation</p>
+
+          <div>
+          {state.imc && state.imc}
+          </div>
+        </div>
+
+        
+
+
+      <ul>
+        <li>Date XX/XX/XXXX imc  interpretation X</li>
+        <li>Date XX/XX/XXXX imc  interpretation X</li>
+      </ul>
     </div>
+
   );
 }
 
